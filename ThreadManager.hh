cd52b3a58f835fbc6b7149a058bf89cadc0b9985
Luc Sinet <luc.sinet@etixgroup.com>
2015-07-14 20:08:58 +0200

    Implemented the Worker in a different file

--- ThreadManager.hh
+++ ThreadManager.hh
@@ -10,70 +10,7 @@
 #include <utility>
 #include <string>
 #include <iostream>
-#include "Task.hpp"
-
-class Worker {
-public:
-  Worker() :
-  thread()
-  , task()
-  , running(false) {
-  }
-
-  ~Worker() {
-    if (this->running)
-      this->stop();
-    if (this->thread.joinable())
-      this->thread.join();
-  };
-
-  void
-  start(std::condition_variable& cv,
-        std::mutex& condvarMutex) {
-    this->running = true;
-    this->thread = std::thread(&Worker::threadMain, this, std::ref(cv), std::ref(condvarMutex));
-  }
-
-  void
-  stop() {
-    this->running = false;
-  }
-
-  void
-  threadMain(std::condition_variable& cv, std::mutex& condvarMutex) {
-    while (this->running) {
-      {
-        std::unique_lock<std::mutex> lock(condvarMutex);
-
-        cv.wait(lock, [this] {
-          return (not this->running || this->task != nullptr);
-        });
-        if (not this->running)
-          return ;
-      }
-      this->task();
-      this->setTask(nullptr);
-    }
-  }
-
-  void
-  setTask(const std::function<void ()>& task) {
-    std::lock_guard<std::mutex> guard(this->mutex);
-    this->task = task;
-  }
-
-  bool
-  isIdle() {
-    std::lock_guard<std::mutex> guard(this->mutex);
-    return (this->task == nullptr);
-  }
-
-private:
-  Task   task;
-  std::thread thread;
-  std::mutex  mutex;
-  bool running;
-};
+#include "Worker.hh"
 
 class ThreadManager {
 public:
