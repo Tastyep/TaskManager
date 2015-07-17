3b55c46fd9ca6f7af077cead3353bf6d9d28650f
Luc Sinet <luc.sinet@gmail.com>
2015-07-17 22:25:17 +0200

    Updated main file, added examples

--- main.cpp
+++ main.cpp
@@ -35,6 +35,28 @@ void test2(ThreadPool& tp) {
   std::cout << std::endl;
 }
 
+void test3(ThreadPool& tp) {
+  std::future<std::string> result;
+  bool stop = false;
+  Task task;
+
+  tp.start();
+  result = task.assign([&stop] {
+    unsigned int sec = 0;
+    while (not stop) {
+      std::this_thread::sleep_for(std::chrono::milliseconds(100));
+      sec += 100;
+      std::cout << "Active for " << sec << " ms" << std::endl;
+    }
+    return std::string("Test 3 passed");
+  });
+  task.setStopFunction([&stop] { stop = true; });
+  tp.addTask(task);
+  std::this_thread::sleep_for(std::chrono::milliseconds(3000));
+  tp.stop();
+  std::cout << result.get() << std::endl;
+}
+
 int main(int argc, char const *argv[]) {
   ThreadManager manager(1);
   ThreadPool tp(1, manager);
@@ -42,5 +64,6 @@ int main(int argc, char const *argv[]) {
   tp.start();
   test1(tp);
   test2(tp);
+  test3(tp);
   return 0;
 }
