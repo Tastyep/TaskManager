6a71fa3713b4fa4eb3b83acca75d57a9fe01af2e
Luc Sinet <luc.sinet@etixgroup.com>
2018-05-01 13:46:20 +0200

    develop #comment Add a basic scheduler which exposes launchIn & launchAt methods.

---
+++ src/Scheduler.cpp
@@ -0,0 +1,63 @@
+#include "Scheduler.hh"
+
+#include <iostream>
+
+namespace Task {
+
+Scheduler::Scheduler(std::shared_ptr<Detail::Threadpool> threadpool, size_t maxWorkers)
+  : _threadpool(std::move(threadpool))
+  , _maxWorkers(maxWorkers) {}
+
+std::future<void> Scheduler::stop(bool discard) {
+  std::lock_guard<std::mutex> guard(_mutex);
+
+  if (discard) {
+    _tasks.clear();
+  }
+  auto task = std::make_shared<std::packaged_task<void()>>([this] {
+    std::unique_lock<std::mutex> guard(_mutex);
+    bool isLast = _workerCount == 0;
+
+    // Guarantee that the task finishes last.
+    while (!isLast) {
+      guard.unlock();
+      std::this_thread::sleep_for(std::chrono::milliseconds(1));
+      guard.lock();
+      isLast = _workerCount == 0;
+    }
+  });
+  auto future = task->get_future();
+  Detail::Timepoint timepoint = Detail::Clock::now();
+
+  if (!_tasks.empty()) {
+    // Set the task just after the last one.
+    timepoint = _tasks.top().timepoint() + std::chrono::nanoseconds(1);
+  }
+  auto functor = [task = std::move(task)]() mutable {
+    (*task)();
+  };
+  // Push the task directly into the pool to avoid id collisions.
+  _threadpool->schedule({ std::move(functor), timepoint });
+
+  return future;
+}
+
+void Scheduler::addTask(const std::string& id, std::function<void()> functor, Detail::Timepoint timepoint) {
+  std::lock_guard<std::mutex> guard(_mutex);
+
+  _tasks.emplace(_hasher(id), std::move(functor), timepoint);
+  this->processTasks();
+}
+
+void Scheduler::processTasks() {
+  if (_tasks.empty() || _workerCount == _maxWorkers) {
+    return;
+  }
+  auto task = std::move(_tasks.top());
+  _tasks.pop();
+
+  ++_workerCount;
+  _threadpool->schedule(task);
+}
+
+} /* namespace Task */
