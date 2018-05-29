8ac9c968ecb5df3fe0de1821a4e8c7db32187349
Luc Sinet <luc.sinet@etixgroup.com>
2018-05-29 22:39:50 +0200

    gh-pages #comment Remove non documentation related files.

--- cmake/CleanCoverage.cmake
+++
@@ -1,8 +0,0 @@
-# Enable CMP0009 new behaviour
-cmake_minimum_required(VERSION 2.6.2 FATAL_ERROR)
-
-# Remove old gcda files
-file(GLOB_RECURSE GCDA_FILES "${PROJECT_BINARY_DIR}/*.gcda")
-if(NOT GCDA_FILES STREQUAL "")
-  file(REMOVE ${GCDA_FILES})
-endif()
